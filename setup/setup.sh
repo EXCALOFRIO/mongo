8d59e23d80a15917ac9a447be2715acd9e98f104
EXCALOFRIO <alejandro.ramirezlarena@usp.ceu.es>
2024-06-03 11:42:37 +0200

    sdf

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-sudo rpm --import https://pgp.mongodb.com/server-7.0.asc
-sudo zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/15/mongodb-org/7.0/x86_64/" mongodb
-sudo zypper -n install mongodb-org
+# Import the MongoDB public GPG key
+wget -qO - https://pgp.mongodb.com/server-7.0.asc | sudo apt-key add -
+
+# Create a list file for MongoDB
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# Reload local package database
+sudo apt-get update
+
+# Install the MongoDB packages
+sudo apt-get install -y mongodb-org
+
+# Start MongoDB
 sudo systemctl start mongod
+
